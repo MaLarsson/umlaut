88354380f4b3299e6b7414ce5edfcfb6eeb63dda
Marcus Larsson <marcuslarsson_92@hotmail.com>
2018-07-07 17:24:42 +0200

    added member functions to umlaut::small_vector_base

--- include/umlaut/small_vector.hpp
+++ include/umlaut/small_vector.hpp
@@ -24,6 +24,8 @@ class small_vector_base {
     using alloc_traits = typename std::allocator_traits<Alloc>::template rebind_traits<T>;
 
 public:
+    /// @name Aliases
+    /// @{
     using value_type = T;
     using allocator_type = typename alloc_traits::allocator_type;
     using size_type = typename alloc_traits::size_type;
@@ -36,8 +38,9 @@ public:
     using const_iterator = const value_type*;
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    /// @}
 
-    /// Constructs the `vector` from a list of values.
+    /// @brief Constructs the `vector` from a list of values.
     template <typename ...Ts>
     small_vector_base(list_construct_t, Ts&&... values) {
 	reserve(sizeof...(values));
@@ -55,14 +58,79 @@ public:
 	(std::apply(forwarding_lambda, tuples), ...);
     }
 
+    /// @name Element access
+    /// @{
+
+    /// @brief Returns element at index `i`.
+    constexpr value_type& operator[](size_type i) { return data()[i]; }
+
+    /// @brief Const overload of `vector::operator[]`.
+    constexpr const value_type& operator[](size_type i) const { return data()[i]; }
+
+    /// @brief Returns a pointer to the underlying data of the `vector`.
+    constexpr value_type* data() noexcept {
+	return static_cast<value_type*>(m_data());
+    }
+
+    /// @brief Const overload of vector::data().
+    constexpr const value_type* data() const noexcept {
+	return static_cast<value_type*>(m_data());
+    }
+    /// @}
+
+    /// @name Iterators
+    /// @{
+    constexpr iterator begin() noexcept { return &data()[0]; }
+    constexpr const_iterator cbegin() noexcept { return &data()[0]; }
+    constexpr const_iterator begin() const noexcept { return &data()[0]; }
+    constexpr iterator end() noexcept { return &data()[m_size]; }
+    constexpr const_iterator cend() noexcept { return &data()[m_size]; }
+    constexpr const_iterator end() const noexcept { return &data()[m_size]; }
+    constexpr reverse_iterator rbegin() noexcept { return &data()[m_size]; }
+    constexpr const_reverse_iterator crbegin() noexcept { return &data()[m_size]; }
+    constexpr const_reverse_iterator rbegin() const noexcept { return &data()[m_size]; }
+    constexpr reverse_iterator rend() noexcept { return &data()[0]; }
+    constexpr const_reverse_iterator crend() noexcept { return &data()[0]; }
+    constexpr const_reverse_iterator rend() const noexcept { return &data()[0]; }
+    /// @}
+
+    /// @name Modifiers
+    /// @{
+
+    /// @brief Adds an element to the end of the `vector`.
+    void push_back(const value_type& value) { emplace_back(value); }
+
+    /// @brief Overload taking an rvalue reference.
+    void push_back(value_type&& value) { emplace_back(std::move(value)); }
+
+    /// @brief Constructs an element in place at the end of the vector by forwarding
+    /// constructor arguments to the `value_type`, returns a reference to the constructed
+    /// element.
+    template <typename ...Args>
+    value_type& emplace_back(Args&&... args) {
+	// TODO ...
+    }
+    /// @}
+
+    /// @name Capacity
+    /// @{
     void reserve(size_type n) {
 	// TODO ...
     }
 
-    template <typename ...Args>
-    void emplace_back(Args&&... args) { // value_type&
-	// TODO ...
-    }
+    /// @brief Returns the numbers of elements in the `vector`.
+    constexpr size_type size() const noexcept { return m_size; }
+
+    /// @brief Returns the potential number of elements in the `vector` before the
+    /// `vector has to reallocate.
+    constexpr size_type capacity() const noexcept { return m_capacity; }
+
+    /// @brief Returns whether the `vector` is empty of not.
+    constexpr bool empty() const noexcept { return m_size == 0; }
+
+    /// @brief Returns the maximum size the can have vector.
+    constexpr size_type max_size() const noexcept { return alloc_traits::max_size(m_alloc()); }
+    /// @}
 
  private:
     compressed_pair<pointer, allocator_type> m_data_and_alloc;
