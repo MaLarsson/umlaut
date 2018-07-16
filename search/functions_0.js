7c9eefb6cffd515b4db73108ca055a62ed82ed58
Travis CI User <travis@example.org>
2018-07-16 14:05:25 +0000

    update documentation to a741b9f

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['capacity',['capacity',['../classumlaut_1_1small__vector__base.html#ab13b2fc71c43b474dc3afb4776534ad7',1,'umlaut::small_vector_base']]],
-  ['compressed_5fpair',['compressed_pair',['../classumlaut_1_1compressed__pair.html#a4da67edb858e58c176f602b7768cd231',1,'umlaut::compressed_pair::compressed_pair()'],['../classumlaut_1_1compressed__pair.html#a5a934cd69c48bf9868edecdaff2d67f5',1,'umlaut::compressed_pair::compressed_pair(T &amp;&amp;first, U &amp;&amp;second)']]]
+  ['compressed_5fpair',['compressed_pair',['../classumlaut_1_1compressed__pair.html#a4da67edb858e58c176f602b7768cd231',1,'umlaut::compressed_pair::compressed_pair()'],['../classumlaut_1_1compressed__pair_a5a934cd69c48bf9868edecdaff2d67f5.html#a5a934cd69c48bf9868edecdaff2d67f5',1,'umlaut::compressed_pair::compressed_pair(T &amp;&amp;first, U &amp;&amp;second)']]]
 ];
