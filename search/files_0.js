8988dc0e45fef5cd0635d3f5885058ff53d487fa
Travis CI User <travis@example.org>
2018-07-21 20:30:06 +0000

    update documentation to dfeb99f

--- search/files_0.js
+++ search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['compressed_5fpair_2ehpp',['compressed_pair.hpp',['../compressed__pair_8hpp.html',1,'']]]
+  ['compressed_5fpair_2ehpp',['compressed_pair.hpp',['../compressed__pair_8hpp.html',1,'']]],
+  ['config_2ehpp',['config.hpp',['../config_8hpp.html',1,'']]]
 ];
