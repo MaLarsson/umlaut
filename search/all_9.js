b693027e0c27fc59a1ff08584eb17e07b7151a6c
Travis CI User <travis@example.org>
2018-08-16 14:16:59 +0000

    update documentation to c526c4c

--- search/all_9.js
+++ search/all_9.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['remove_5fcvref',['remove_cvref',['../structumlaut_1_1remove__cvref.html',1,'umlaut']]],
   ['remove_5fcvref_5ft',['remove_cvref_t',['../structumlaut_1_1remove__cvref_a3c0600da43d6867a247432fcc43ce84f.html#a3c0600da43d6867a247432fcc43ce84f',1,'umlaut::remove_cvref']]],
-  ['reserve',['reserve',['../classumlaut_1_1small__vector__base.html#a4aabd898fbf4f1e98a69f53494a4a8db',1,'umlaut::small_vector_base']]]
+  ['reserve',['reserve',['../classumlaut_1_1small__vector__base_a4aabd898fbf4f1e98a69f53494a4a8db.html#a4aabd898fbf4f1e98a69f53494a4a8db',1,'umlaut::small_vector_base']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['reserve',['reserve',['../classumlaut_1_1small__vector__base.html#a4aabd898fbf4f1e98a69f53494a4a8db',1,'umlaut::small_vector_base']]]
+  ['reserve',['reserve',['../classumlaut_1_1small__vector__base_a4aabd898fbf4f1e98a69f53494a4a8db.html#a4aabd898fbf4f1e98a69f53494a4a8db',1,'umlaut::small_vector_base']]]
 ];
