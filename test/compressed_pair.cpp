bb45b71bd540f4aa68156674cf45490f46df5a03
Marcus Larsson <marcuslarsson_92@hotmail.com>
2018-09-28 17:00:37 +0200

    migrated from googletest to catch2 for ul::compressed_pair tests

--- test/compressed_pair.cpp
+++ test/compressed_pair.cpp
@@ -2,30 +2,24 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 
-#include <gtest/gtest.h>
+#include <catch2/catch.hpp>
 #include <umlaut/compressed_pair.hpp>
 #include <string>
 
-namespace {
+struct empty_cat { std::string purr() const { return "purr"; } };
 
-struct empty_cat {
-    std::string purr() const { return "purr"; }
-};
+TEST_CASE("access functions for compressed_pair", "[compressed_pair]") {
+    ul::compressed_pair<int, empty_cat> pair{42, {}};
 
-TEST(compressedPair, size) {
+    CHECK(pair.first() == 42);
+    CHECK(pair.second().purr() == "purr");
+}
+
+TEST_CASE("empty base optimization of compressed_pair", "[compressed_pair]") {
     ul::compressed_pair<int, int> largest{1, 1};
     ul::compressed_pair<int, empty_cat> middle{1, {}};
     ul::compressed_pair<empty_cat, empty_cat> smallest{{}, {}};
 
-    EXPECT_TRUE(sizeof(largest) > sizeof(middle));
-    EXPECT_TRUE(sizeof(middle) > sizeof(smallest));
+    CHECK(sizeof(largest) > sizeof(middle));
+    CHECK(sizeof(middle) > sizeof(smallest));
 }
-
-TEST(compressedPair, access) {
-    ul::compressed_pair<int, empty_cat> pair{42, {}};
-
-    EXPECT_EQ(pair.first(), 42);
-    EXPECT_EQ(pair.second().purr(), "purr");
-}
-
-} // namespace
